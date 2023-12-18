433f9ec57c5dbbc33d9a06af455b8a734328a799
marlonbossa09 <marlonbossa@gmail.com>
2023-12-18 01:13:44 -0500

    RestauranteCambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { ThreeSceneComponent } from './three-scene/three-scene.component';
 // RUTAS
 import { APP_ROUTING } from './app.routes';
 
 import { HeroesService } from './components/services/heroes.service';
-
+import { ThreeComponent } from './three/three.component';
+import { Modelo3DComponent } from './components/home/modelo3D.component';
 // SERVICIOS
 
 @NgModule({
@@ -20,7 +22,10 @@ import { HeroesService } from './components/services/heroes.service';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    ThreeSceneComponent,
+    ThreeComponent,
+    Modelo3DComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/home/modelo3D.component.ts
@@ -0,0 +1,76 @@
+import { Component, NgZone, OnInit, OnDestroy } from '@angular/core';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { AmbientLight } from 'three';
+
+@Component({
+  selector: 'app-modelo3d',
+  template: '<div id="modelo3D"></div>',
+})
+export class Modelo3DComponent implements OnInit, OnDestroy {
+  private scene: THREE.Scene;
+  private camera: THREE.PerspectiveCamera;
+  private renderer: THREE.WebGLRenderer;
+  private modelo: any | THREE.Object3D;
+
+  constructor(private ngZone: NgZone) {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000); // Adjust the aspect ratio
+    this.renderer = new THREE.WebGLRenderer({ alpha: true }); // Set alpha to true for a transparent background
+  }
+
+  ngOnInit() {
+    this.init();
+    this.loadModel();
+    this.animate();
+  }
+
+  ngOnDestroy() {
+    // No se necesita disposición especial sin OrbitControls
+  }
+
+  private init() {
+    // Configurar la cámara y el renderizador
+    this.camera.position.z = 3;
+    this.renderer.setSize(window.innerWidth, window.innerHeight * 0.8);
+
+    const ambientLight = new AmbientLight(0xffffff, 0.5); // Luz ambiental
+    this.scene.add(ambientLight);
+
+    // Agregar el renderizador al contenedor HTML
+    const contenedor = document.getElementById('modelo3D');
+    contenedor!.appendChild(this.renderer.domElement);
+
+    // Ajustar el tamaño del renderizador al contenedor
+    this.renderer.setSize(contenedor!.clientWidth, contenedor!.clientHeight);
+  }
+
+  private loadModel() {
+    // Cargar y agregar el modelo 3D aquí
+    const loader = new GLTFLoader();
+    loader.load('assets/img/cuy_frito.glb', (gltf) => {
+      this.modelo = gltf.scene;
+      this.modelo.rotation.x = Math.PI / 3;
+      this.modelo.position.set(0, 1, 0);
+      this.modelo.scale.set(1, 1, 1);
+      this.scene.add(this.modelo);
+    });
+  }
+
+  private animate() {
+    this.ngZone.runOutsideAngular(() => {
+      const animateFn = () => {
+        requestAnimationFrame(animateFn);
+
+        // Agregar rotación al modelo
+        if (this.modelo) {
+          this.modelo.rotation.y += 0.001; // Ajusta la velocidad de rotación según tus necesidades
+        }
+
+        this.renderer.render(this.scene, this.camera);
+      };
+
+      animateFn();
+    });
+  }
+}
